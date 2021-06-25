7dc0fe06f27fb42b5927aae3fb439256f435a471
Am0k-GIT <b1gb0ss@mail.ru>
2021-06-26 00:33:49 +0300

    -

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 
 using namespace std;
 
-char passLib[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char fileName[] = "results.txt";
+const char passLibType1[] = "0123456789";
+const char passLibType2[] = "abcdefghijklmnopqrstuvwxyz";
+const char passLibType3[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const char passLibType4[] = "!@#$%^&*()_+-=/.,<>`~";
+const char fileName[] = "results.txt";
 
 int getNumber(int minValue, int maxValue) {
     while (true)
@@ -34,9 +37,9 @@ int getRandomNumber(int min, int max)
 
 double getVariants(int passLength, int libLength)
 {
-    if (passLength == 0)
+    if (passLength == 1)
         return libLength;
-    return passLength * getVariants(passLength - 1, libLength);
+    return libLength * getVariants(passLength - 1, libLength);
 }
 
 void main() {
@@ -46,8 +49,42 @@ void main() {
     cout << "Enter a password count ( 1 - 1024 ): ";
     int passCount = getNumber(1, 1024);
     cout << "Password count : " << passCount << endl;
+    cout << "Select password type ( 1 - 4 ): " << endl;
+    cout << "1. \"0123456789\"" << endl;
+    cout << "2. \"0123456789abc\"" << endl;
+    cout << "3. \"0123456789abcABC\"" << endl;
+    cout << "4. \"0123456789abcABC!@#$%\"" << endl;
+    int passType = getNumber(1, 4);
+    char passLib[128] = "";
+    int libLength;
+    switch (passType)
+    {
+    case 1:
+        libLength = sizeof(passLibType1) / sizeof(char) - 1;
+        strcat_s(passLib, passLibType1);
+        break;
+    case 2:
+        libLength = (sizeof(passLibType1) + sizeof(passLibType2)) / sizeof(char) - 2;
+        strcat_s(passLib, passLibType1);
+        strcat_s(passLib, passLibType2);
+        break;
+    case 3:
+        libLength = (sizeof(passLibType1) + sizeof(passLibType2) + sizeof(passLibType3)) / sizeof(char) - 3;
+        strcat_s(passLib, passLibType1);
+        strcat_s(passLib, passLibType2);
+        strcat_s(passLib, passLibType3);
+        break;
+    case 4:
+        libLength = (sizeof(passLibType1) + sizeof(passLibType2) + sizeof(passLibType3) + sizeof(passLibType4)) / sizeof(char) - 4;
+        strcat_s(passLib, passLibType1);
+        strcat_s(passLib, passLibType2);
+        strcat_s(passLib, passLibType3);
+        strcat_s(passLib, passLibType4);
+        break;
+    }
+    cout << "Password type : " << passType << endl;
     srand(static_cast<unsigned int>(time(0)));
-    int libLength = sizeof(passLib) / sizeof(char) - 1;
+    cout << "Library length : " << libLength << endl;
     cout << "Your passwords : " << endl;
     ofstream outFile;
     outFile.open(fileName);
